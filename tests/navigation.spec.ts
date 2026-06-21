a24509f62e19097ba019b3bf5d5d133159dc3ffe
liyicuba <liyicuba@hotmail.com>
2026-06-21 20:32:42 -0300

    Reto 4- Navegar el panel izquierdo

--- tests/navigation.spec.ts
+++ tests/navigation.spec.ts
@@ -1,27 +1,27 @@
 import {test, expect} from '@playwright/test';
 
-test('Navigate to Admin Page', async ({page}) => {
+test('Check left menu items options', async ({page}) => {
 
     await page.goto('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
     await page.getByRole('textbox', {name: 'Username'}).fill('Admin')
     await page.getByRole('textbox', {name: 'Password'}).fill('admin123')
     await page.getByRole('button', {name: 'Login'}).click()
+
     await expect(page.getByRole('link', {name: 'Admin'})).toBeVisible()
 
-    const LeftMenuItems = await page.getByLabel('sidepanel').getByRole('listitem')
+    const LeftMenuItems = page.getByLabel('sidepanel').getByRole('listitem')
     const currentMenuItemscount = await LeftMenuItems.count()
     console.log('Current menu items count', currentMenuItemscount)
 
     const currentMenuItemText :string[] = []
 
-    for (let i = 0; i < currentMenuItemscount; i++) {
+     for (let i = 0; i < currentMenuItemscount; i++) {
+        const menuItem =  LeftMenuItems.nth(i)
+        const menuText = await menuItem.innerText()
+        currentMenuItemText.push(menuText)
+     }
 
-        const menuItemText = await LeftMenuItems.nth(i).innerText()
-        currentMenuItemText.push(menuItemText)
-    }
-    console.log('Current menu items text', currentMenuItemText)
-
-        const expectedMenuItems = [
+           const expectedMenuItems = [
             'Admin',
             'PIM', 
             'Leave', 
@@ -40,5 +40,51 @@ test('Navigate to Admin Page', async ({page}) => {
     console.log('First menu item text', currentMenuItemText[0])
     expect(currentMenuItemText[0]).toEqual("Admin")
 
-});
 
+})
+
+test('Navigate through left menu items', async ({page}) => {
+
+    await page.goto('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
+    await page.getByRole('textbox', {name: 'Username'}).fill('Admin')
+    await page.getByRole('textbox', {name: 'Password'}).fill('admin123')
+    await page.getByRole('button', {name: 'Login'}).click()
+
+    await expect(page.getByRole('link', {name: 'Admin'})).toBeVisible()
+
+    const LeftMenuItems = page.getByLabel('sidepanel').getByRole('listitem')
+    const currentMenuItemscount = await LeftMenuItems.count()
+    //console.log('Current menu items count', currentMenuItemscount)
+
+    //const currentMenuItemText :string[] = []
+
+    for (let i = 0; i < currentMenuItemscount; i++) {
+
+        const menuItem =  LeftMenuItems.nth(i)
+        
+        const menuText = await menuItem.innerText()
+
+        console.log('current menu item text', menuText)
+
+         /*if (menuText === 'Maintenance') {
+        await page.getByRole('button', {name: 'Cancel'}).click();
+        continue;
+    }*/
+   if (menuText === 'Maintenance') {
+    await menuItem.click()
+    await page.getByRole('button', { name: 'Cancel' }).click()
+    await page.waitForLoadState('networkidle').catch(() => {})
+    continue
+}
+
+
+}
+
+     
+    
+
+       
+});
+   
+
+   
