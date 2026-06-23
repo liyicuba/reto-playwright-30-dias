18a2b6a2d7d7d41c8417615b22d84c77ea1639a8
liyicuba <liyicuba@hotmail.com>
2026-06-23 17:50:14 -0300

    Reto 5 - Navegar por los links del Topbar

--- tests/navigation.spec.ts
+++ tests/navigation.spec.ts
@@ -66,25 +66,92 @@ test('Navigate through left menu items', async ({page}) => {
 
         console.log('current menu item text', menuText)
 
-         /*if (menuText === 'Maintenance') {
-        await page.getByRole('button', {name: 'Cancel'}).click();
-        continue;
-    }*/
-   if (menuText === 'Maintenance') {
-    await menuItem.click()
-    await page.getByRole('button', { name: 'Cancel' }).click()
-    await page.waitForLoadState('networkidle').catch(() => {})
-    continue
-}
-
-
-}
-
+        
+        if (menuText === 'Maintenance') {
+        await menuItem.click()
+        await page.getByRole('button', { name: 'Cancel' }).click()
+        await page.waitForLoadState('networkidle').catch(() => {})
+        continue
+        }
+    }
+})
      
+test('Check all the qualifications links', async ({page}) => {
+
     
+    const expectedPages = [
+        {Menu:'Skills',
+         url: '/web/index.php/admin/viewSkills'  
+        },
+
+        {Menu: 'Education',
+         url: '/web/index.php/admin/viewEducation'
+        },
+        {Menu: 'Licenses',
+        url: '/web/index.php/admin/viewLicenses'
+        }
+        ]  
+
+    await page.goto('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
+    await page.getByRole('textbox', {name: 'Username'}).fill('Admin')
+    await page.getByRole('textbox', {name: 'Password'}).fill('admin123')
+    await page.getByRole('button', {name: 'Login'}).click()
+
+    await expect(page.getByRole('link', {name: 'Admin'})).toBeVisible()
+    await page.getByRole('link', {name: 'Admin'}).click()
+    await page.getByRole('navigation', {name: 'Topbar menu'}).getByText('Qualifications').click()
+
+    const qualificationOptions = page.getByRole('menu').locator('li')
+    const qualificationOptionsCount = await qualificationOptions.count()
+
+    for (let expectedPage of expectedPages) {
+        const menuOption = qualificationOptions.filter({hasText: expectedPage.Menu})
+        await menuOption.click()
+        await expect(page).toHaveURL(new RegExp(expectedPage.url))
+        await page.getByRole('navigation', {name: 'Topbar menu'}).getByText('Qualifications').click()
+
+
+    }
 
        
 });
-   
 
-   
+test('Check all the Organization links', async ({page}) => {
+
+    
+    const expectedPages = [
+        {Menu:'General Information',
+         url: '/web/index.php/admin/viewOrganizationGeneralInformation'  
+        },
+
+        {Menu: 'Locations',
+         url: '/web/index.php/admin/viewLocations'
+        },
+        {Menu: 'Structure',
+        url: '/web/index.php/admin/viewCompanyStructure'
+        }
+        ]  
+
+    await page.goto('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
+    await page.getByRole('textbox', {name: 'Username'}).fill('Admin')
+    await page.getByRole('textbox', {name: 'Password'}).fill('admin123')
+    await page.getByRole('button', {name: 'Login'}).click()
+
+    await expect(page.getByRole('link', {name: 'Admin'})).toBeVisible()
+    await page.getByRole('link', {name: 'Admin'}).click()
+    await page.getByRole('navigation', {name: 'Topbar menu'}).getByText('Organization').click()
+
+    const organizationOptions = page.getByRole('menu').locator('li')
+    const organizationOptionsCount = await organizationOptions.count()
+
+    for (let expectedPage of expectedPages) {
+        const menuOption = organizationOptions.filter({hasText: expectedPage.Menu})
+        await menuOption.click()
+        await expect(page).toHaveURL(new RegExp(expectedPage.url))
+        await page.getByRole('navigation', {name: 'Topbar menu'}).getByText('Organization').click()
+
+
+    }
+
+       
+});
