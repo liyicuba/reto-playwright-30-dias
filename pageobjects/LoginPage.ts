5d2f4b7da998f8ee74df4154aefe722a53aaf85d
liyicuba <liyicuba@hotmail.com>
2026-07-01 17:39:38 -0300

    Reto 15 - contraseñas Random y validacion de error

--- pageobjects/LoginPage.ts
+++ pageobjects/LoginPage.ts
@@ -32,4 +32,70 @@ export class LoginPage {
         await this.doLogin(Environment.EMPLOYEE_USERNAME, Environment.EMPLOYEE_PASSWORD);
     }
 
+    async loginWithWrongCredentials() {
+        await this.doLogin(Environment.EMPLOYEE_USERNAME, Environment.WRONG_PASSWORD);
+    }
+
+    async nuevoEmployee(){
+
+        await this.page.getByRole('button', { name: 'Add' }).click();
+        await this.page.locator("//label[contains(.,'User Role')]/parent::div/following-sibling::div").click();
+        await this.page.getByRole('listbox').getByRole('option', { name: 'ESS' }).click();
+
+        const employeeNameInput = this.page.getByRole('textbox', { name: 'Type for hints...' });
+        await employeeNameInput.click();
+        await employeeNameInput.fill('Test')
+        await this.page.waitForTimeout(1000);
+
+        // Esperar que aparezca el listbox de sugerencias
+        const suggestions = this.page.getByRole('listbox').getByRole('option');
+        await suggestions.first().waitFor();
+
+        // Seleccionar la primera opción
+        await suggestions.first().click();
+        await this.page.locator("//label[contains(.,'Status')]/parent::div/following-sibling::div").click()
+        await this.page.getByRole('listbox').getByRole('option', { name: 'Enabled' }).click();
+
+        const usernameInput = this.page.locator("//label[contains(.,'Username')]/parent::div/following-sibling::div//input")
+        await usernameInput.fill(Environment.EMPLOYEE_USERNAME + crypto.randomUUID().slice(0, 5)); // Generar un nombre de usuario único
+
+        const passwordFields = this.page.locator("//label[contains(.,'Password')]/parent::div/following-sibling::div//input")
+        await passwordFields.nth(0).fill(Environment.EMPLOYEE_PASSWORD); // Password
+        await passwordFields.nth(1).fill(Environment.EMPLOYEE_PASSWORD); // Confirm Password
+            
+        await this.page.getByRole('button', {name: 'Save'}).click()
+        
+        }
+
+    async nuevowrongEmployee(){
+
+        await this.page.getByRole('button', { name: 'Add' }).click();
+        await this.page.locator("//label[contains(.,'User Role')]/parent::div/following-sibling::div").click();
+        await this.page.getByRole('listbox').getByRole('option', { name: 'ESS' }).click();
+
+        const employeeNameInput = this.page.getByRole('textbox', { name: 'Type for hints...' });
+        await employeeNameInput.click();
+        await employeeNameInput.fill('Test')
+        await this.page.waitForTimeout(1000);
+
+        // Esperar que aparezca el listbox de sugerencias
+        const suggestions = this.page.getByRole('listbox').getByRole('option');
+        await suggestions.first().waitFor();
+
+        // Seleccionar la primera opción
+        await suggestions.first().click();
+        await this.page.locator("//label[contains(.,'Status')]/parent::div/following-sibling::div").click()
+        await this.page.getByRole('listbox').getByRole('option', { name: 'Enabled' }).click();
+
+        const usernameInput = this.page.locator("//label[contains(.,'Username')]/parent::div/following-sibling::div//input")
+        await usernameInput.fill(Environment.EMPLOYEE_USERNAME + crypto.randomUUID().slice(0, 5)); // Generar un nombre de usuario único
+
+        const passwordFields = this.page.locator("//label[contains(.,'Password')]/parent::div/following-sibling::div//input")
+        await passwordFields.nth(0).fill(Environment.EMPLOYEE_PASSWORD); // Password
+        await passwordFields.nth(1).fill(Environment.EMPLOYEE_PASSWORD + crypto.randomUUID().slice(0, 5)); // Confirm Password
+            
+       // await this.page.getByRole('button', {name: 'Save'}).click()
+        
+        }
+
 }
--- tests/loginOrange.spec.ts
+++ tests/loginOrange.spec.ts
@@ -2,11 +2,14 @@ import { test, expect } from '@playwright/test';
 import { LoginPage } from '../pageobjects/LoginPage';
 import { SidePanel, SidePanelItems } from '../components/SidePanel';
 import { SearchInput } from '../components/SearchInput';
+import { Environment } from '../config/Environment';
 
 
 test('Login OrangeHRM', async ({ page }) => {
-    const loginPage = new LoginPage(page);
-    await loginPage.loginasAdmin();
+    /*const loginPage = new LoginPage(page);
+    await loginPage.loginasAdmin();*/
+
+    await page.goto('/web/index.php/dashboard/index')
 
     const sidePanel = new SidePanel(page);
     await sidePanel.clickOnOption(SidePanelItems.Admin);
@@ -16,25 +19,61 @@ test('Login OrangeHRM', async ({ page }) => {
 
 test('Search on SidePanel', async ({ page }) => {
     const loginPage = new LoginPage(page);
-    await loginPage.doLogin('Admin', 'admin123');
+    await loginPage.loginasAdmin()
 
     const searchInput = new SearchInput(page);
     await searchInput.searchAndValidate(SidePanelItems.Admin);
 });
 
 
-test('Invalid Login OrangeHRM', async ({ page }) => {
-    const loginPage = new LoginPage(page);
-    await loginPage.doLogin('Admin', 'wrongpassword');
-
-    await expect(page.getByText('Invalid credentials')).toBeVisible()
-});
-
-
 test('Login as Employee OrangeHRM', async ({ page }) => {
+
     const loginPage = new LoginPage(page);
     await loginPage.loginasEmployee();
 
     await expect(page.getByRole('link', {name: 'Admin'})).not.toBeVisible()
+
+    
+
+})
+
+test('Invalid Login OrangeHRM', async ({ page }) => {
+    const loginPage = new LoginPage(page);
+    await loginPage.loginWithWrongCredentials();
+
+    await expect(page.getByText('Invalid credentials')).toBeVisible()
+});
+
+test('Crear un nuevo usuario', async ({ page }) => {
+
+    const loginPage = new LoginPage(page);
+    await loginPage.loginasAdmin();
+
+    const sidePanel = new SidePanel(page);
+    await sidePanel.clickOnOption(SidePanelItems.Admin);
+
+    await loginPage.nuevoEmployee();
+
+    await expect(page.locator('p.oxd-text--toast-message')).toHaveText('Successfully Saved')
+          
+
+
+})
+
+test('Crear un nuevo usuario con distinta contraseña', async ({ page }) => {
+
+    const loginPage = new LoginPage(page);
+    await loginPage.loginasAdmin();
+
+    const sidePanel = new SidePanel(page);
+    await sidePanel.clickOnOption(SidePanelItems.Admin);
+
+    await loginPage.nuevowrongEmployee();
+
+
+    const passwordsDoNotMatch = page.locator('//div[contains(@class, "user-password-row")]//span[contains(@class, "error")]') 
+    //console.log('Texto completo del contenedor:', await passwordMismatchError.textContent());
+    await expect(passwordsDoNotMatch).toHaveText('Passwords do not match')
+    
 
 })
