9c42fb381dd41039cd047fbae3bf43ed2a987eba
liyicuba <liyicuba@hotmail.com>
2026-07-01 12:22:38 -0300

    Reto 13- Employee SetUp

---
+++ tests/auth.setup.ts
@@ -0,0 +1,44 @@
+import {test as setup, expect } from '@playwright/test'
+import { LoginPage } from '../pageobjects/LoginPage'
+
+setup ('authentication as admin', async ({page}) => {
+
+    console.log('autenticacion usando el setup')
+    //inicio de sesion
+
+    const loginPage = new LoginPage(page)
+    await loginPage.loginasAdmin()
+
+    //Nos aseguramos que el inicio de sesion es exitoso
+
+    await expect(page.getByRole('link', {name: 'Admin'})).toBeVisible()
+
+    //Guardar el estado
+
+    await page.context().storageState({ path: '.auth/admin.json' })
+
+    console.log('Autenticacion completada usando el setup')
+
+
+})
+
+setup ('authentication as employee', async ({page}) => {
+
+    console.log('autenticacion usando el setup')
+    //inicio de sesion
+
+    const loginPage = new LoginPage(page)
+    await loginPage.loginasEmployee()
+
+    //Nos aseguramos que el inicio de sesion es exitoso
+
+    await expect(page.getByRole('link', {name: 'Admin'})).not.toBeVisible()
+
+    //Guardar el estado
+
+    await page.context().storageState({ path: '.auth/employee.json' })
+
+    console.log('Autenticacion completada usando el setup')
+
+
+})
